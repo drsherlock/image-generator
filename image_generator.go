11e54cf4b04b41c41e78ed22bc281fed6093f621
drsherlock <zain.delhi27@hotmail.com>
2021-05-09 00:34:02 +0530

    Refactor

--- image_generator.go
+++ image_generator.go
@@ -11,6 +11,15 @@ import (
 	"sync"
 )
 
+type Image struct {
+	fileName   string
+	title      string
+	titleColor string
+	fontName   string
+	width      float64
+	height     float64
+}
+
 func main() {
 	// get file name, title from arguments
 	fileName := flag.String("file-name", "input.jpg", "name of image")
@@ -18,66 +27,26 @@ func main() {
 	titleColor := flag.String("title-color", "#ffffff", "color of title")
 	flag.Parse()
 
-	if err := run(fileName, title, titleColor); err != nil {
+	image := Image{
+		fileName:   *fileName,
+		title:      *title,
+		titleColor: *titleColor,
+	}
+
+	if err := run(image); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
 }
 
-func run(fileName *string, title *string, titleColor *string) error {
-	// draw the image
-	backgroundImage, err := gg.LoadImage(*fileName)
-
-	if err != nil {
-		return fmt.Errorf("load background image %w", err)
-	}
-
-	// add text
-	textShadowColor := color.Black
-
+func run(image Image) error {
 	var wg sync.WaitGroup
-	err = filepath.Walk("fonts", func(path string, info os.FileInfo, err error) error {
-		if !info.IsDir() {
+	err := filepath.Walk("fonts", func(fontPath string, fontInfo os.FileInfo, err error) error {
+		image.fontName = fontInfo.Name()
+
+		if !fontInfo.IsDir() {
 			wg.Add(1)
-			go func() error {
-				defer wg.Done()
-
-				dc := gg.NewContextForImage(backgroundImage)
-
-				backgroundImage = imaging.Fill(backgroundImage, dc.Width(), dc.Height(), imaging.Center, imaging.Lanczos)
-
-				dc.DrawImage(backgroundImage, 0, 0)
-
-				// draw the overlay
-				imageWidth := float64(dc.Width())
-				imageHeight := float64(dc.Height())
-
-				x := imageWidth / 50
-				y := imageHeight / 50
-				w := imageWidth - (2.0 * x)
-				h := imageHeight - (2.0 * y)
-				dc.SetColor(color.RGBA{0, 0, 0, 150})
-				dc.DrawRectangle(x, y, w, h)
-				dc.Fill()
-				if err := dc.LoadFontFace(path, 200); err != nil {
-					return fmt.Errorf("load font face %w", err)
-				}
-				textMargin := x * 1.5
-				textTopMargin := imageHeight / 2.5
-				x = textMargin
-				y = textTopMargin
-				maxWidth := imageWidth - textMargin - textMargin
-
-				dc.SetColor(textShadowColor)
-				dc.DrawStringWrapped(*title, x+1, y+1, 0, 0, float64(maxWidth), 1.5, gg.AlignCenter)
-				dc.SetHexColor(*titleColor)
-				dc.DrawStringWrapped(*title, x, y, 0, 0, float64(maxWidth), 1.5, gg.AlignCenter)
-
-				if err := dc.SavePNG("output/" + info.Name() + ".png"); err != nil {
-					return fmt.Errorf("save png %w", err)
-				}
-				return nil
-			}()
+			go generate(image, fontPath, &wg)
 		}
 		return nil
 	})
@@ -88,7 +57,80 @@ func run(fileName *string, title *string, titleColor *string) error {
 		return fmt.Errorf("load font face %w", err)
 	}
 
-	// fontPath := filepath.Join("fonts", "BebasNeue-Regular.ttf")
+	return nil
+}
+
+func generate(image Image, fontPath string, wg *sync.WaitGroup) error {
+	defer wg.Done()
+
+	backgroundImage, err := gg.LoadImage(image.fileName)
+	if err != nil {
+		return fmt.Errorf("load background image %w", err)
+	}
+	dc := gg.NewContextForImage(backgroundImage)
+
+	image.width = float64(dc.Width())
+	image.height = float64(dc.Height())
+
+	// draw the image
+	drawImage(image, dc)
+
+	// draw the overlay
+	drawOverlay(image, dc)
+
+	// add text
+	err = addText(image, dc, fontPath)
+	if err != nil {
+		return fmt.Errorf("add text %w", err)
+	}
+
+	// save image
+	err = saveImage(image, dc)
+	if err != nil {
+		return fmt.Errorf("save image %w", err)
+	}
+
+	return nil
+}
+
+func drawImage(image Image, dc *gg.Context) {
+	backgroundImage := imaging.Fill(dc.Image(), dc.Width(), dc.Height(), imaging.Center, imaging.Lanczos)
+	dc.DrawImage(backgroundImage, 0, 0)
+}
+
+func drawOverlay(image Image, dc *gg.Context) {
+	x := image.width / 50
+	y := image.height / 50
+	w := image.width - (2.0 * x)
+	h := image.height - (2.0 * y)
+	dc.SetColor(color.RGBA{0, 0, 0, 150})
+	dc.DrawRectangle(x, y, w, h)
+	dc.Fill()
+}
+
+func addText(image Image, dc *gg.Context, fontPath string) error {
+	textShadowColor := color.Black
+	if err := dc.LoadFontFace(fontPath, 200); err != nil {
+		return err
+	}
+	textMargin := image.width * 0.03
+	textTopMargin := image.height / 2.5
+	x := textMargin
+	y := textTopMargin
+	maxWidth := image.width - textMargin - textMargin
+
+	dc.SetColor(textShadowColor)
+	dc.DrawStringWrapped(image.title, x+1, y+1, 0, 0, float64(maxWidth), 1.5, gg.AlignCenter)
+	dc.SetHexColor(image.titleColor)
+	dc.DrawStringWrapped(image.title, x, y, 0, 0, float64(maxWidth), 1.5, gg.AlignCenter)
+
+	return nil
+}
+
+func saveImage(image Image, dc *gg.Context) error {
+	if err := dc.SavePNG("output/" + image.fontName + ".png"); err != nil {
+		return err
+	}
 
 	return nil
 }
